20e62c1fc7868040e4ce5d535ffd7c7ddbeb7103
Naomie <syntichekotin@gmail.com>
2024-01-22 12:38:57 +0100

    singlescrowllview

--- extract_texte/lib/display_screen.dart
+++ extract_texte/lib/display_screen.dart
@@ -15,31 +15,33 @@ class DisplayScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Image et texte détecté'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Afficher l'image capturée
-            Image.file(
-              File(imagePath),
-              width: 300, // Ajustez la taille selon vos besoins
-              height: 300,
-            ),
-            SizedBox(height: 20),
-
-            // Afficher le texte détecté
-            Text(
-              'Texte détecté :',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              extractedText,
-              style: TextStyle(fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding( 
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Afficher l'image capturée
+              Image.file(
+                File(imagePath),
+                width: 300,
+                height: 300,
+              ),
+              SizedBox(height: 20),
+        
+              // Afficher le texte détecté
+              Text(
+                'Texte détecté :',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                extractedText,
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
